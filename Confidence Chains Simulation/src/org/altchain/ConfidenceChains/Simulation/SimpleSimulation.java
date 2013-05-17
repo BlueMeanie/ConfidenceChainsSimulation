fc9a2b1f72cd4a43c621cf286d77432654f03daf
BlueMeanie <josh.josh421@gmail.com>
2013-05-17 11:00:13 -0400

    initial commit

---
+++ Confidence Chains Simulation/src/org/altchain/ConfidenceChains/Simulation/SimpleSimulation.java
@@ -0,0 +1,13 @@
+package org.altchain.ConfidenceChains.Simulation;
+
+public class SimpleSimulation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
